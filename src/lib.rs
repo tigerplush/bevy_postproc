1b377c2cc154c97bf0c64d1ac41c07a0aff9215e
tigerplush <64077786+tigerplush@users.noreply.github.com>
2023-12-24 15:46:42 +0100

    Added minimal example and basic structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use bevy::{prelude::*, render::{RenderApp, render_resource::{BindGroupLayout, Sampler, CachedRenderPipelineId, BindGroupLayoutDescriptor, BindGroupLayoutEntry, ShaderStages, BindingType, TextureSampleType, TextureViewDimension, SamplerBindingType, SamplerDescriptor, PipelineCache, RenderPipelineDescriptor, FragmentState, ColorTargetState, TextureFormat, ColorWrites, PrimitiveState, MultisampleState}, renderer::RenderDevice, texture::BevyDefault}, core_pipeline::fullscreen_vertex_shader::fullscreen_shader_vertex_state};
+
+pub mod prelude {
+    pub use bevy::render::extract_component::ExtractComponent;
+    pub use bevy::render::render_resource::ShaderType;
+
+    pub use crate::PostProcessingPlugin;
+    pub use crate::PostProcessingEffect;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Clone, Copy, Debug)]
+pub struct PostProcessingEffect {
+    pub path: &'static str,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// Postprocessing plugin for bevy.
+pub struct PostProcessingPlugin {
+    effects: Vec<PostProcessingEffect>,
+}
+
+impl PostProcessingPlugin {
+    /// Creates a new postprocessing pipeline.
+    pub fn new() -> Self {
+        Self {
+            effects: Vec::new(),
+        }
+    }
+
+    pub fn with(&mut self, effect: PostProcessingEffect) -> &mut Self {
+        self.effects.push(effect);
+        self
+    }
+
+    pub fn build(&self) -> Self {
+        Self {
+            effects: self.effects.clone()
+        }
+    }
+}
+
+impl From<&mut Self> for PostProcessingPlugin {
+    fn from(value: &mut Self) -> Self {
+        value.into()
+    }
+}
+
+impl Plugin for PostProcessingPlugin {
+    fn build(&self, app: &mut App) {
+        info!("Building plugin...");
+        for effect in self.effects.clone() {
+        }
     }
 }
